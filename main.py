cb6c92215331b9ff353efb41abf3749f248123dd
prashant <pmani157@gmail.com>
2022-12-09 11:20:06 +0530

    Pydantic input validation updated

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
+from typing import Optional
 from fastapi import FastAPI
 from fastapi.params import Body
 from pydantic import BaseModel
 
 app = FastAPI()
 
-
+# pydantic model for input validation. 
 class Post(BaseModel):
     title: str
     content: str
+    publish: bool = True #if user does not specify publish value, default will be true.
+    rating: Optional[int] = None
 
 @app.get("/how")
 async def root():
@@ -18,7 +21,9 @@ def get_post():
     return {"data": "This is a sample post"}
 
 @app.post("/create_post")
-def createpost(new_post: Post):
-    print("title: " +new_post.title)
-    return{"data": "new_post created"}
+def createpost(user_post: Post):
+    print(user_post.rating)
+    # printing the pydantic instance into dictionary
+    print(user_post.dict())
+    return{"data": user_post}
 

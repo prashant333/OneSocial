4ad23192911e74ea1b041fdc19b1ac5cc493c12b
prashant <pmani157@gmail.com>
2024-04-28 12:28:19 +0530

    create an in-memory list of posts

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from typing import Optional
 from fastapi import FastAPI
 from fastapi.params import Body
 from pydantic import BaseModel
+from random import randrange
 
 app = FastAPI()
 
@@ -12,18 +13,21 @@ class Post(BaseModel):
     publish: bool = True #if user does not specify publish value, default will be true.
     rating: Optional[int] = None
 
+my_post = [{"titile": "title 1", "content":"content 1", "publish":"True", "id":"1"},
+           {"titile": "title 2", "content":"content 2", "publish":"False", "id":"2"}]
+
 @app.get("/login")
 async def root():
     return {"message": "Welcome to my new api!!!"}
 
 @app.get("/post")
 def get_post():
-    return {"data": "This is a sample post"}
+    return {"data": my_post}
 
 @app.post("/create_post")
 def createpost(user_post: Post):
-    print(user_post.rating)
-    # printing the pydantic instance into dictionary
-    print(user_post.dict())
-    return{"data": user_post}
+    post_data = user_post.dict()
+    post_data['id'] = randrange(0,100000)
+    my_post.append(post_data)
+    return{"data": my_post}
 

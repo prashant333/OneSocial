ce5d3d742fb75234c04e2091726553338a1ceca4
prashant <pmani157@gmail.com>
2024-05-23 08:20:29 +0530

    post class modified to postbase in schema file

--- app/schema.py
+++ app/schema.py
@@ -3,9 +3,10 @@ from datetime import datetime
 from typing import Optional
 
 # pydantic model for input validation. 
-class Post(BaseModel):
+class PostBase(BaseModel):
     title: str
     content: str
     published: bool = True
 
-
+class PostCreate(PostBase):
+    pass

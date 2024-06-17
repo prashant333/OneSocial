dd6d306671625d4eeb2fd6733e23c23c4f6b88a0
prashant <pmani157@gmail.com>
2024-06-17 17:17:04 +0530

    response error code changed to forbidden in case of invalid credetials

--- app/oauth2.py
+++ app/oauth2.py
@@ -1,5 +1,11 @@
 from jose import JWTError, jwt
 from datetime import datetime, timedelta
+import schema
+from fastapi import Depends, status, HTTPException
+from fastapi.security import OAuth2PasswordBearer
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl='login')
+
 SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
 ALGORITHAM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
@@ -13,3 +19,26 @@ def create_access_token(data: dict):
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHAM)
 
     return encoded_jwt
+
+def verify_access_token(token: str, credentials_exception):
+
+    try:
+
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHAM])
+
+        id: str =payload.get("user_id")
+
+        if id is None:
+            raise credentials_exception
+        token_data = schema.TokenData(id=id)
+
+    except JWTError:
+        raise credentials_exception
+    
+    return token_data
+    
+    def get_current_user(token: str= Depends(oauth2_scheme)):
+        credentials_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, details="session timeout",
+                                              headers={"WWW-Authenticate":"Bearer"})
+        
+        return verify_access_token(token, credentials_exception)

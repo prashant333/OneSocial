29a37c7c9a8c19e475c093f8f4955cfe920c3a33
prashant <pmani157@gmail.com>
2024-06-17 16:49:32 +0530

    using the oauth2passwordrequestform for storing user credentials

--- app/router/auth.py
+++ app/router/auth.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Depends, status, HTTPException, Response
+from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 from database import get_db
 import schema, models, utils, oauth2
@@ -6,8 +7,9 @@ import schema, models, utils, oauth2
 router = APIRouter(tags=['Authentication'])
 
 @router.post('/login')
-def login(user_credentials:schema.UserLogin, db:Session = Depends(get_db)):
-    user = db.query(models.User).filter(models.User.email == user_credentials.email).first()
+def login(user_credentials:OAuth2PasswordRequestForm = Depends(), db:Session = Depends(get_db)):
+    
+    user = db.query(models.User).filter(models.User.email == user_credentials.username).first()
 
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="invalid credentials")

439d015e4d8399390ab99bd5e72d37f2b0cd75a7
prashant <pmani157@gmail.com>
2024-06-20 08:15:13 +0530

    http exception changed in case of invalid credentials

--- app/router/auth.py
+++ app/router/auth.py
@@ -12,10 +12,10 @@ def login(user_credentials:OAuth2PasswordRequestForm = Depends(), db:Session = D
     user = db.query(models.User).filter(models.User.email == user_credentials.username).first()
 
     if not user:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="invalid credentials")
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="invalid credentials")
     
     if not utils.verify(user_credentials.password, user.password):
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="invalid credentials")
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="invalid credentials")
     
     #creating a JWT token
     access_token = oauth2.create_access_token(data={"user_id":user.id})

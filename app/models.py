cf500871366a6bb86939afb4bfb47db53247b7e1
prashant <pmani157@gmail.com>
2024-07-02 10:44:39 +0530

    added users table in databasae with foreign key relatation to posts table

--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
 from database import Base
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
 from sqlalchemy.sql.expression import text
 from sqlalchemy.sql.sqltypes import TIMESTAMP
 
@@ -11,6 +11,7 @@ class Post(Base):
     content = Column(String, nullable=False)
     published = Column(Boolean, server_default='False', nullable=False)
     created_at = Column(TIMESTAMP(timezone=True),nullable=False, server_default=text('now()'))
+    owner_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
 
 class User(Base):
     __tablename__ = "users"

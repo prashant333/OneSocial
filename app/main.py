8a640860d636952bae6dff049db27fa223217739
prashant <pmani157@gmail.com>
2024-08-04 15:25:10 +0530

    code cleanup on main file

--- app/main.py
+++ app/main.py
@@ -1,14 +1,10 @@
-from typing import Optional, List
-from fastapi import FastAPI, Response, status, HTTPException, Depends
+from fastapi import FastAPI
 from fastapi.params import Body
-from pydantic import BaseModel
-from random import randrange
 import psycopg2
 from psycopg2.extras import RealDictCursor
 import time
-from sqlalchemy.orm import Session
-import models, schema, utils
-from database import engine, get_db
+import models
+from database import engine
 from router import post, user, auth
 
 models.Base.metadata.create_all(bind=engine)
@@ -28,18 +24,6 @@ app = FastAPI()
 #         print("Error:", error)
 #         time.sleep(4)
 
-my_post = [{"titile": "title 1", "content":"content 1", "publish":"True", "id":"1"},
-           {"titile": "title 2", "content":"content 2", "publish":"False", "id":"2"}]
-
-def find_post(id):
-    for p in my_post:
-        if p["id"] == id:
-            return p
-        
-def find_index(id):
-    for i, p in enumerate(my_post):
-        if p['id'] == id:
-            return i
         
 app.include_router(post.router)
 app.include_router(user.router)

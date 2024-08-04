736db3dabd18ab1df7fc02b14c7627a9d8d1b251
prashant <pmani157@gmail.com>
2024-08-04 14:55:33 +0530

    added owner info in the post output response

--- app/schema.py
+++ app/schema.py
@@ -11,10 +11,19 @@ class PostBase(BaseModel):
 class PostCreate(PostBase):
     pass
 
+class UserOut(BaseModel):
+    id:int
+    email: EmailStr
+    created_at: datetime
+
+    class Config:
+        orm_mode=True
+
 class PostResponse(PostBase):
     id:int
     created_at: datetime
     owner_id:int
+    owner:UserOut
     class Config:
         orm_mode = True
 
